aa961204958d83d951ac807673235b4301851f21
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-02-21 15:09:32 +0000

    Creating Dates

--- 12.Numbers:Dates/script.js
+++ 12.Numbers:Dates/script.js
@@ -359,3 +359,41 @@ console.log(10000n + 100000n);
 */
 
 //Create a date
+
+// const now = new Date();
+// console.log(now);
+
+// console.log(new Date("February 14, 2023"));
+
+// console.log(new Date(account1.movementsDates[0]));
+
+// console.log(new Date(2037, 10, 19, 15, 23, 5));
+// console.log(new Date(2037, 10, 31));
+
+// console.log(new Date(0));
+// console.log(new Date(3 * 24 * 60 * 60 * 1000));
+
+//Working with Dates
+
+const future = new Date(2037, 10, 19, 15, 23, 5);
+console.log(future);
+
+console.log(future.getFullYear());
+console.log(future.getMonth());
+console.log(future.getDate());
+console.log(future.getDay());
+console.log(future.getHours());
+console.log(future.getMinutes());
+console.log(future.getSeconds());
+
+console.log(future.toISOString());
+
+console.log(future.getTime());
+//TimeStamp
+console.log(new Date(2142256985000));
+
+console.log(Date.now());
+
+//
+future.setFullYear(2040);
+console.log(future);
