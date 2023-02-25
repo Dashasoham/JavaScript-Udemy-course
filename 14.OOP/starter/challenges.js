e368ff7045d4489623869465490f8e78e10e308b
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-02-25 15:54:23 +0000

    ES6 classes

---
+++ 14.OOP/starter/challenges.js
@@ -0,0 +1,31 @@
+'use strict';
+/*
+
+const Car = function (make, speed) {
+  this.make = make;
+  this.speed = speed;
+
+  //Never do this
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
+};
+
+const car1 = new Car('BMW', 120);
+const car2 = new Car('Mercedez', 95);
+
+Car.prototype.accelerate = function () {
+  this.speed += 10;
+  console.log(this.speed);
+};
+
+Car.prototype.break = function () {
+  this.speed -= 5;
+  console.log(this.speed);
+};
+car1.accelerate();
+car1.accelerate();
+car1.break();
+car2.accelerate();
+car2.break();
+*/
--- 14.OOP/starter/script.js
+++ 14.OOP/starter/script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 const Person = function (firstName, birthYear) {
   this.firstName = firstName;
   this.birthYear = birthYear;
@@ -42,7 +42,76 @@ console.log(jonas.species);
 console.log(jonas.hasOwnProperty('firstName'));
 console.log(jonas.hasOwnProperty('species'));
 
-const arr = [2, 6, 36, 673, 7];
+const arr = [2, 6, 36, 673, 7, 6];
 console.log(arr.__proto__);
 
-// Array.prototype.
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+*/
+//Class declaration
+// class PersonCl {}
+//Class expression
+const PersonCl = class {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+  //Methods will be added to .prototype property
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  }
+  greet() {
+    console.log(`Hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2037 - this.birthYear;
+  }
+  //Set a property that already exists
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not a full name`);
+  }
+  get fullName() {
+    return this._fullName;
+  }
+};
+
+const jessica = new PersonCl('Jessica Davis', 1996);
+
+console.log(jessica);
+jessica.calcAge();
+console.log(jessica.age);
+
+console.log(jessica.__proto__ === PersonCl.prototype);
+
+// PersonCl.prototype.greet = function () {
+//   console.log(`Hey ${this.firstName}`);
+// };
+jessica.greet();
+
+//1.Classes are not hoisted
+//2. Classes are 1-class citizens
+//3. Classes are executed in strict mode
+
+const walter = new PersonCl('Walter White', 1965);
+console.log(walter);
+const account = {
+  owner: 'Jonas',
+  movements: [200, 520, 243, 250],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(account.latest);
+account.latest = 50;
+console.log(account.movements);
