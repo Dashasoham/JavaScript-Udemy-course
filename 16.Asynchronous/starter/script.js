72c77d80b24e0e41760f100f9d14807e08c9f128
Daria Korniienko <106033233+Dashasoham@users.noreply.github.com>
2023-03-27 14:39:53 +0100

    promisifying geoloc API

--- 16.Asynchronous/starter/script.js
+++ 16.Asynchronous/starter/script.js
@@ -117,7 +117,9 @@ console.log("test start");
 setTimeout(() => console.log("0 sec timer"), 0);
 Promise.resolve("Resolved promise 1").then((res) => console.log(res));
 console.log("test end");
-*/
+
+
+
 
 const lotteryPromise = new Promise(function (resolve, reject) {
   console.log("Lottery draw is happening");
@@ -150,3 +152,78 @@ wait(2)
 
 Promise.resolve("abc").then((x) => console.log(x));
 Promise.reject("abc").catch((x) => console.error(x));
+*/
+const btn = document.querySelector(".btn-country");
+
+const countriesContainer = document.querySelector(".countries");
+const renderCountry = function (data, className) {
+  const html = `
+      <article class="country ${className}">
+     <img class="country__img" src="${
+       data.flag
+     }" />  <div class="country__data">
+        <h3 class="country__name">${data.name}</h3>
+        <h4 class="country__region">${data.region}</h4>
+        <p class="country__row"><span>👫</span>${+(
+          data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+        <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+      </div>
+    </article>`;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+};
+
+const renderError = function (msg) {
+  countriesContainer.insertAdjacentText("beforeend", msg);
+  countriesContainer.style.opacity = 1;
+};
+
+console.log("Getting position");
+
+const getPosition = function () {
+  return new Promise(function (resolve, reject) {
+    // navigator.geolocation.getCurrentPosition(
+    //   (position) => resolve(position),
+    //   (err) => reject(err)
+    // );
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+};
+
+// getPosition().then((pos) => console.log(pos));
+
+const whereAmI = function () {
+  getPosition()
+    .then((pos) => {
+      const { latitude: lat, longitude: longt } = pos.coords;
+      console.log(pos.coords);
+      return fetch(`https://geocode.xyz/${lat},${longt}?geoit=json`);
+    })
+
+    .then((result) => {
+      if (!result.ok)
+        throw new Error(`Problem with geocoding ${result.status}`);
+      console.log(result);
+      return result.json();
+    })
+    .then((data) => {
+      console.log(data);
+      console.log(`You're in ${data.city}, ${data.country}`);
+
+      return fetch(`https://restcountries.com/v2/name/${data.country}`);
+    })
+    .then((result) => {
+      if (!result.ok) throw new Error(`Country not found(${result.status})`);
+      return result.json();
+    })
+    .then((data) => renderCountry(data[0]))
+    .catch((err) => console.error(`${err.message}💥💥💥`));
+  // .catch((err) => {
+  //   console.log(`${err}`);
+  // });
+};
+
+btn.addEventListener("click", whereAmI);
